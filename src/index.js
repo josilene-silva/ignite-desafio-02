b284adbbd25685a589975f5a6b6a85da66874a2a
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-05 23:20:10 -0400

    implementa checksTodoExists

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "User not found"
+    });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "UUID not valid"
+    });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({
+      error: "Todo not found"
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
